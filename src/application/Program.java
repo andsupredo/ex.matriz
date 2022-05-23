5e714534e9c8e645e91f43a0d7c42aa7b3e8037f
andsu <andersoo.sousa@gmail.com>
2022-05-23 17:11:16 -0300

    inittial commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -16,20 +15,20 @@ public class Program {
         System.out.print("Digite o número de colunas: ");
         int n = sc.nextInt();
 
-        int [][] mat = new int[m][n];
+        int [][] mat = new int[m][n]; /*Instanciação da matriz com o nome "mat"*/
 
-        for (int i=0; i<mat.length; i++){
+        for (int i=0; i<mat.length; i++){ /*utilizando o mat.length para o laço utilizar o numero de linhas em i e colunas em j*/
             System.out.print("Digite a linha " + i + ": ");
             for (int j=0; j<mat.length; j++){
-                mat[i][j] = sc.nextInt();
+                mat[i][j] = sc.nextInt(); /*Utilizar o mat[][] = sc... para adicionar os numeros digitados à matriz*/
             }
         }
         System.out.print("Digite um número inteiro: ");
-        int x = sc.nextInt();
+        int x = sc.nextInt(); /*Variável será utilizada para verificar igualiadde na matriz, cada vez que for encontrado o valor = x será impresso em tela os valores abaixo, a direita, acima e a esquerda.*/
 
         for (int i=0; i<mat.length; i++){
-            for (int j=0; j<mat.length; j++){
-                if (mat[i][j] == x){
+            for (int j=0; j<mat.length; j++){ /*Laços for para passar por toda a extensão da matriz*/
+                if (mat[i][j] == x){ /*igualidade da matriz*/
                     x++;
                     System.out.println("Posição " + i + "," + j + ":");
                     if (j > 0) {
@@ -43,7 +42,7 @@ public class Program {
                     }
                     if (i < mat[j].length-1) {
                         System.out.println("Abaixo: " + mat[i+1][j]);
-                    }
+                    } /*Maneira de definir os números nas proximidades do valor = x*/
                 }
             }
         }
